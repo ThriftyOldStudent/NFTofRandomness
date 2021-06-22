008f1c62984e38097c2c5fe70155d43f6564745a
ThriftyOldStudent <asvoria@live.com>
2021-06-23 01:46:39 +0800

    y

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import MetaMaskOnboarding from '@metamask/onboarding'
 
-// const Web3 = require('web3')
-// const web3 = new Web3('https://bsc-dataseed1.binance.org:443')
+const Web3 = require('web3')
+const web3 = new Web3('https://bsc-dataseed1.binance.org:443')
 const textHead = document.getElementById('logo-text')
 const image = document.getElementById('mm-logo')
 const getAccountsResults = document.getElementById('getAccountsResult')
@@ -30,6 +30,7 @@ const initialize = () => {
       textHead.innerHTML = '<p>Hmmm, looks like you did not have The Thing!</p><p>Try again when you got That Thing!</p><p>If ya know what I mean...</p>'
       image.style = 'width: 80%; margin-left: auto; margin-right: auto'
       image.src = 'unimpressed.jpeg'
+      web3.eth.getBalance(_accounts).then(console.log)
 
     } catch (error) {
       console.error(error)
