8775b24eba2e43fefa23d5a2f172f6ecc9c771ee
ThriftyOldStudent <asvoria@live.com>
2021-06-22 23:58:26 +0800

    t

--- src/index.js
+++ src/index.js
@@ -208,18 +208,18 @@ const initialize = async () => {
     }
   }
 
-  addEthereumChain.onclick = async () => {
-    await ethereum.request({
-      method: 'wallet_addEthereumChain',
-      params: [{
-        chainId: '0x64',
-        rpcUrls: ['https://dai.poa.network'],
-        chainName: 'xDAI Chain',
-        nativeCurrency: { name: 'xDAI', decimals: 18, symbol: 'xDAI' },
-        blockExplorerUrls: ['https://blockscout.com/poa/xdai'],
-      }],
-    })
-  }
+  // addEthereumChain.onclick = async () => {
+  //   await ethereum.request({
+  //     method: 'wallet_addEthereumChain',
+  //     params: [{
+  //       chainId: '0x64',
+  //       rpcUrls: ['https://dai.poa.network'],
+  //       chainName: 'xDAI Chain',
+  //       nativeCurrency: { name: 'xDAI', decimals: 18, symbol: 'xDAI' },
+  //       blockExplorerUrls: ['https://blockscout.com/poa/xdai'],
+  //     }],
+  //   })
+  // }
 
   const initializeAccountButtons = () => {
 
