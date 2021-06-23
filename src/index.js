aa1596074f36e69a16189b75a95d301f40af84d1
ThriftyOldStudent <asvoria@live.com>
2021-06-23 21:46:17 +0800

    e

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ const initialize = () => {
       const ownerOfAddress = await ERC721Contract.methods.ownerOf('26403').call() // get the owner of the NFT
       console.log('ownerOfAddress: ')
       console.log(ownerOfAddress)
-      if (_accounts[0] === '0x4D3343C4e99aD0c63335171834f24267cf1c0831') {
+      if (_accounts[0] == '0x4D3343C4e99aD0c63335171834f24267cf1c0831') {
         textHead.innerHTML = '<p>You owned my Nativity NFT!</p><p>Thanks for your support!</p><p>May Baby Jesus bless you with greatness!!!</p>'
         textMore.innerHTML = '<p>I had received total of 0.95BAKE from your purchase.</p><p>Thank you for your generous support.</p><p><a href="https://github.com/ThriftyOldStudent/NFTofRandomness" target="_blank">You can find the source code of this webApp at github!</a></p>'
         image.style = 'width: 80%; margin-left: auto; margin-right: auto'
