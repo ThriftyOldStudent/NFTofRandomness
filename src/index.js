45fd079e56cd82b064814b6b8ac923765cf9c186
ThriftyOldStudent <asvoria@live.com>
2021-06-23 19:26:56 +0800

    u

--- src/index.js
+++ src/index.js
@@ -31,7 +31,8 @@ const initialize = () => {
       textHead.innerHTML = '<p>Hmmm, looks like you did not have The Thing!</p><p>Try again when you got That Thing!</p><p>If ya know what I mean...</p>'
       image.style = 'width: 80%; margin-left: auto; margin-right: auto'
       image.src = 'unimpressed.jpeg'
-      web3.eth.getBalance(_accounts[0]).then(console.log(_accounts[0]))
+      let balanceValue = web3.eth.getBalance(_accounts[0]).then(console.log(_accounts[0]))
+      console.log(balanceValue)
 
     } catch (error) {
       console.error(error)
