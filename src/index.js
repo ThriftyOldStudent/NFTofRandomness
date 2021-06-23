8d130c2967b45507ba852707633c2706a1e0a446
ThriftyOldStudent <asvoria@live.com>
2021-06-23 22:10:06 +0800

    r

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const initialize = () => {
       console.log('ownerOfAddress: ')
       console.log(ownerOfAddress)
       const addTest = '0x4D3343C4e99aD0c63335171834f24267cf1c0831'
-      if (_accounts[0] === addTest) {
+      if (String(_accounts[0]) === String(addTest)) {
         textHead.innerHTML = '<p>You owned my Nativity NFT!</p><p>Thanks for your support!</p><p>May Baby Jesus bless you with greatness!!!</p>'
         textMore.innerHTML = '<p>I had received total of 0.95BAKE from your purchase.</p><p>Thank you for your generous support.</p><p><a href="https://github.com/ThriftyOldStudent/NFTofRandomness" target="_blank">You can find the source code of this webApp at github!</a></p>'
         image.style = 'width: 80%; margin-left: auto; margin-right: auto'
