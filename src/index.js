321d0dda05714bdf1e4b758a1b05262282e6a3ec
ThriftyOldStudent <asvoria@live.com>
2021-06-23 21:14:32 +0800

    i

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import MetaMaskOnboarding from '@metamask/onboarding'
 
 const ERC721ABI = [{
   'constant': true,
-  'inputs': [{ 'name':'tokenId', 'type':'uint256' }],
+  'inputs': [{ 'name': 'tokenId', 'type': 'uint256' }],
   'name': 'ownerOf',
-  'outputs': [{ 'name':'owner', 'type':'address' }],
+  'outputs': [{ 'name': 'owner', 'type': 'address' }],
   'type': 'function'
-}];
+}]
 
 const Web3 = require('web3')
 
