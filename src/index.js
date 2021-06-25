bd38761f0a6d3983bc5d6f5b971550c7f729070e
ThriftyOldStudent <asvoria@live.com>
2021-06-26 01:07:05 +0800

    y

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ const textHead = document.getElementById('logo-text')
 const textMore = document.getElementById('msg-more')
 const image = document.getElementById('mm-logo')
 const getAccountsResults = document.getElementById('getAccountsResult')
-const ERC721ContractBAKE = new web3.eth.Contract(ERC721ABI, '0x5bc94e9347f3b9be8415bdfd24af16666704e44f')
+const ERC721ContractANFT = new web3.eth.Contract(ERC721ABI, '0x5bc94e9347f3b9be8415bdfd24af16666704e44f')
 const ERC721ContractFCC = new web3.eth.Contract(ERC721ABI, '0x2d956093d27621ec0c4628b77eaeac6c734da02c')
+const ERC721ContractBBNFT = new web3.eth.Contract(ERC721ABI, '0x43DB8ea81074b31Cf2665B600A4086cF36B59445')
 
 const currentUrl = new URL(window.location.href)
 const forwarderOrigin = currentUrl.hostname === 'localhost'
@@ -38,14 +39,14 @@ const initialize = () => {
         method: 'eth_accounts',
       })
       getAccountsResults.innerHTML = _accounts[0] || 'Not able to get accounts'
-      const ownerOfAddressBAKE = await ERC721ContractBAKE.methods.ownerOf('26403').call()
+      const ownerOfAddressANFT = await ERC721ContractANFT.methods.ownerOf('26403').call()
       const ownerOfAddressFCC = await ERC721ContractFCC.methods.ownerOf('3436').call()
-      const ownerOfAddressBAKE2 = await ERC721ContractBAKE.methods.ownerOf('3436').call()
+      const ownerOfAddressBBNFT = await ERC721ContractBBNFT.methods.ownerOf('3310').call()
       const userAccount = _accounts[0]
-      const checkBAKE = await (userAccount.toLowerCase() === ownerOfAddressBAKE.toLowerCase())
+      const checkANFT = await (userAccount.toLowerCase() === ownerOfAddressANFT.toLowerCase())
       const checkFCC = await (userAccount.toLowerCase() === ownerOfAddressFCC.toLowerCase())
-      const checkBAKE2 = await (userAccount.toLowerCase() === ownerOfAddressBAKE2.toLowerCase())
-      if (checkBAKE) {
+      const checkBBNFT = await (userAccount.toLowerCase() === ownerOfAddressBBNFT.toLowerCase())
+      if (checkANFT) {
         textHead.innerHTML = '<p>You owned my Nativity NFT!</p><p>Thanks for your support!</p><p>May Baby Jesus bless you with greatness!!!</p>'
         textMore.innerHTML = '<p>I had received total of 0.95BAKE from your purchase.</p><p>Thank you for your generous support.</p><p><a href="https://github.com/ThriftyOldStudent/NFTofRandomness" target="_blank">You can find the source code of this webApp at github!</a></p>'
         image.style = 'width: 80%; margin-left: auto; margin-right: auto'
@@ -55,7 +56,7 @@ const initialize = () => {
         textMore.innerHTML = '<p>Whatever is the reason, you are definately a whale!</p><p>Thank you for your generous support!</p><p>Now I\'m able to pay off my PHD tuition!</p><p><a href="https://github.com/ThriftyOldStudent/NFTofRandomness" target="_blank">You can find the source code of this webApp at github!</a></p>'
         image.style = 'width: 70%; margin-left: auto; margin-right: auto'
         image.src = 'why_meme.jpeg'
-      } else if (checkBAKE2) {
+      } else if (checkBBNFT) {
         textHead.innerHTML = '<p>You owned my Chinese Peony NFT!</p><p>Thanks for your support!</p><p>Now you should learn more about the real NFT!!!</p>'
         textMore.innerHTML = '<p>It is a form of programmable unique asset!</p><p>Checkout the most basic implementation of this WebApp!</p><p><a href="https://github.com/ThriftyOldStudent/NFTofRandomness" target="_blank">You can find the source code of this webApp at github!</a></p>'
         image.style = 'width: 80%; margin-left: auto; margin-right: auto'
