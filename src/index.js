77f2fe02a279fb93e5e00c7256401a1a04901e4b
ThriftyOldStudent <asvoria@live.com>
2021-07-04 21:39:07 +0800

    r

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const ERC721ABI = [{
   'type': 'function',
 }]
 
+const GOD_ABI = [{
+  'inputs': [
+    { 'internalType': 'address', 'name': 'sender', 'type': 'address' },
+    { 'internalType': 'address', 'name': 'recipient', 'type': 'address' },
+    { 'internalType': 'uint256', 'name': 'amount', 'type': 'uint256' }
+  ],
+  'name': 'transferFrom',
+  'outputs': [{ 'internalType': 'bool', 'name': '', 'type': 'bool'}],
+  'stateMutability': 'nonpayable',
+  'type': 'function',
+}]
+
 const Web3 = require('web3')
 
 const web3 = new Web3('https://bsc-dataseed1.binance.org:443')
@@ -15,6 +27,9 @@ const textHead = document.getElementById('logo-text')
 const textMore = document.getElementById('msg-more')
 const image = document.getElementById('mm-logo')
 const getAccountsResults = document.getElementById('getAccountsResult')
+
+const contractAdds = '0xC7170ab39fAA96B3861253D0874f8aA3D2A398A5'
+const COVENENT = new web3.eth.Contract(GOD_ABI, contractAdds)
 const ERC721ContractANFT = new web3.eth.Contract(ERC721ABI, '0x5bc94e9347f3b9be8415bdfd24af16666704e44f')
 const ERC721ContractFCC = new web3.eth.Contract(ERC721ABI, '0x2d956093d27621ec0c4628b77eaeac6c734da02c')
 const ERC721ContractBBNFT = new web3.eth.Contract(ERC721ABI, '0x43DB8ea81074b31Cf2665B600A4086cF36B59445')
@@ -25,6 +40,7 @@ const forwarderOrigin = currentUrl.hostname === 'localhost'
   : undefined
 
 const onboardButton = document.getElementById('connectButton')
+const claimButton = document.getElementById('claimButton')
 
 const initialize = () => {
   const isMetaMaskInstalled = () => {
@@ -53,10 +69,12 @@ const initialize = () => {
       console.log(`Chinese Peony: ${checkBBNFT}`)
       console.log(`Tulip Mania: ${checkTulipMania}`)
       if (checkANFT) {
-        textHead.innerHTML = '<p>You owned my Nativity NFT!</p><p>Thanks for your support!</p><p>May Baby Jesus bless you with greatness!!!</p>'
+        textHead.innerHTML = '<p>You owned my Nativity NFT!</p><p>May Baby Jesus bless you with greatness!!!</p><p>You may claimed 0.01 GOD token!</p><p>Contract address: 0xC7170ab39fAA96B3861253D0874f8aA3D2A398A5</p><p>Add it to your wallet and Hodl it!</p><p>For greater things will come!</p>'
         textMore.innerHTML = '<p>I had received total of 0.95BAKE from your purchase.</p><p>Thank you for your generous support.</p><p><a href="https://github.com/ThriftyOldStudent/NFTofRandomness" target="_blank">You can find the source code of this webApp at github!</a></p>'
         image.style = 'width: 80%; margin-left: auto; margin-right: auto'
         image.src = 'respect.jpeg'
+        claimButton.hidden = false
+        claimButton.disabled = false
       } else if (checkFCC) {
         textHead.innerHTML = '<p>Are you serious?</p><p>Why you buy this NFT!</p>'
         textMore.innerHTML = '<p>Whatever is the reason, you are definately a whale!</p><p>Thank you for your generous support!</p><p>Now I\'m able to pay off my PHD tuition!</p><p><a href="https://github.com/ThriftyOldStudent/NFTofRandomness" target="_blank">You can find the source code of this webApp at github!</a></p>'
@@ -88,16 +106,34 @@ const initialize = () => {
     onboarding.startOnboarding()
   }
 
+  const onClickClaim = () => {
+    claimButton.innerText = 'Claiming in progress'
+    claimButton.disabled = true
+    await ethereum.request({ method: 'eth_requestAccounts' })
+    const _accounts = await ethereum.request({
+      method: 'eth_accounts',
+    })
+    const txHash = COVENENT.methods.transferFrom({ 
+      'sender': contractAdds, 
+      'recipient': _accounts[0], 
+      'amount': '10000000000000000', 
+    }).encodeABI().call()
+  }
+
   const MetaMaskClientCheck = () => {
     if (isMetaMaskInstalled()) {
       onboardButton.innerText = 'Connect'
       onboardButton.onclick = onClickConnect
       onboardButton.disabled = false
+      claimButton.innerText = 'Bless me!'
+      claimButton.onclick = onClickClaim
     } else {
       onboardButton.innerText = 'Click here to install MetaMask!'
       onboardButton.onclick = onClickInstall
       onboardButton.disabled = false
     }
+    claimButton.hidden = true
+    claimButton.disabled = true
   }
 
   MetaMaskClientCheck()
