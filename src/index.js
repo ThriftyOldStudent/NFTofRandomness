f15068cbe9432843ee699b7fac6464e69ad34ff1
ThriftyOldStudent <asvoria@live.com>
2021-07-04 21:42:51 +0800

    r

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const GOD_ABI = [{
   'inputs': [
     { 'internalType': 'address', 'name': 'sender', 'type': 'address' },
     { 'internalType': 'address', 'name': 'recipient', 'type': 'address' },
-    { 'internalType': 'uint256', 'name': 'amount', 'type': 'uint256' }
+    { 'internalType': 'uint256', 'name': 'amount', 'type': 'uint256' },
   ],
   'name': 'transferFrom',
   'outputs': [{ 'internalType': 'bool', 'name': '', 'type': 'bool'}],
